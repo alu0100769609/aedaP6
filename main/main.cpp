d7692865b3d3b40e279ecfd34907759dc7684757
Adexe Sabina <alu0100769609@ull.edu.es>
2016-05-11 18:44:03 +0100

    Creado main vacío

---
+++ main/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>   // system()
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+   system("clear");
+   cout << "This program has not been finished" << endl;
+   cin.ignore();                    // Discard input
+   system("clear");
+}
